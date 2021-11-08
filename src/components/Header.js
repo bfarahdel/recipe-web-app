0b92ea86d830aa334cec899ecd5918b908479389
Jaier Gordon <jaiergordon@MacBook-Pro-2.local>
2021-11-08 12:50:35 -0500

    added the navbar component and replaced the result list with a Bootstrap ListGroup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,52 @@
+import {
+  Navbar, Nav, NavDropdown,
+} from 'react-bootstrap';
+
 function Header() {
+  const testData = [
+    {
+      name: 'PIZZA',
+      calores: 550,
+    },
+    {
+      name: 'NY PIZZA',
+      calories: 200,
+    },
+    {
+      name: 'DEEP DISH PIZZA',
+      calories: 203,
+    },
+  ];
+
+  const renderFavs = () => testData.map((fav) => (
+    <NavDropdown.Item as="li" href="">{fav.name}</NavDropdown.Item>
+  ));
+
   return (
     <div>
-      {/* Nav Bar */}
-      <h1>  RECIPE FINDER </h1>
+      <Navbar collapseOnSelect bg="light" expand="xl" className="navBar">
+        {/* <Container fluid> */}
+          <Navbar.Brand href="#" className="navTitle">
+            <h1>
+            RECIPE FINDER
+            </h1>
+          </Navbar.Brand>
+            <Navbar.Text expand='lg' className="justify">
+              <Nav
+                className="me-auto my-2 my-lg-3 alignRight"
+                style={{ maxHeight: '170px' }}
+                navbarScroll
+              >
+                <Nav.Link href="" className="navLink">HOME</Nav.Link>
+                <NavDropdown title="FAVS" className="navLink" id="navbarScrollingDropdown">
+                  {renderFavs()}
+                </NavDropdown>
+                <Nav.Link href="#" className="navLink">
+                  PROFILE
+                </Nav.Link>
+              </Nav>
+            </Navbar.Text>
+      </Navbar>
     </div>
   );
 }
