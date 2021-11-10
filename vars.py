ad5d26ee944f0dc22d6e43f9bb63a9520c249c86
Shreshth Kaushik <shreshthsoccer12@gmail.com>
2021-11-10 12:36:31 -0500

    DOne

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
-import flask
+import flask, os
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from dotenv import load_dotenv, find_dotenv
@@ -9,7 +9,7 @@ load_dotenv(find_dotenv())
 APP = flask.Flask(__name__, static_folder="./build/static")
 APP.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 APP.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-APP.secret_key = b"any_random_key"
+APP.secret_key = os.getenv("secret_key")
 
 db = SQLAlchemy(APP)
 bcrypt = Bcrypt(APP)
