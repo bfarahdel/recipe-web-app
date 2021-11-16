f5b155e04c065ab79af0f67d506c8ec6c5628d52
bfarahdel <britny205@gmail.com>
2021-11-15 23:35:52 -0500

    Class methods return booleans instead of print statements.

--- validations.py
+++ validations.py
@@ -1,16 +1,22 @@
+"""Validates if the user entered a correct email and username during login"""
+
+
 class Validation:
+    """Validates if the user used the correct login credentials"""
+
     def __init__(self, word):
         self.word = word
 
     def validation_email(self):
+        """Validates the user's email"""
         if self.word.find("@") == -1:
-            print("False")
+            return False
 
-        else:
-            print("True")
+        return True
 
     def validation_username(self):
+        """Validates the user's username"""
         if len(self.word) > 10:
-            print("Word is too large")
-        else:
-            print("OK")
+            return False
+
+        return True
